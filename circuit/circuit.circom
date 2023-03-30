5032c34b4bc7bc514479368c4a55243bd8f65ac2
CreepyK19 <kaffelmahmoud@gmail.com>
2023-03-30 05:56:41 +0300

    raw commit

--- circuit/circuit.circom
+++ circuit/circuit.circom
@@ -7,10 +7,16 @@ template Example() {
     signal input b;
     signal input c;
     signal input enforce;
+    signal input d;
+    signal input f;
+    signal output out1;
+    signal output out2;
 
     component if_enforce = ForceEqualIfEnabled();
     if_enforce.in[0] <== a + b;
     if_enforce.in[1] <== c;
     if_enforce.enabled <== enforce;
 
+    out1 <== a + b;
+    out2 <== d + f;
 }
--- tests/test.ts
+++ tests/test.ts
@@ -2,7 +2,7 @@ let circomlibjs = require("circomlibjs");
 const calculateWtns = require("../build/Circuit_js/witness_calculator.js");
 var ffjavascript = require("ffjavascript");
 const { unstringifyBigInts, stringifyBigInts, leInt2Buff, leBuff2int } = ffjavascript.utils;
-import {  readFileSync} from "fs";
+import { readFileSync } from "fs";
 const snarkjs = require("snarkjs");
 
 describe("Tests", () => {
@@ -14,10 +14,12 @@ describe("Tests", () => {
 
 
         const inputs = {
-            a: "1",
             b: "2",
+            a: "1",
             c: "3",
-            enforce: "1"
+            enforce: "1",
+            d: "3",
+            f: "2"
         }
 
         let witnessCalculator = await calculateWtns(buffer);
@@ -28,25 +30,30 @@ describe("Tests", () => {
             inputs,
             0,
         );
-
+        //console.log('wtns: ', wtns);
         const { proof, publicSignals } = await snarkjs.groth16.prove(
             completePathZkey,
             wtns,
         );
         console.timeEnd("Proof generation");
         console.log("publicSignals", publicSignals);
-        console.log("proof", proof);
+        //console.log("proof", proof);
 
         const vKey = await snarkjs.zKey.exportVerificationKey(
             completePathZkey,
           );
-          const res = await snarkjs.groth16.verify(vKey, publicSignals, proof);
-          if (res === true) {
-            console.log("Verification OK");
-          } else {
-            console.log("Invalid proof");
-            throw new Error("Invalid Proof");
-          }
+        const r1cs = await snarkjs.r1cs.exportJson('./build/Circuit.r1cs');
+        console.log('r1cs json: ', r1cs);
+        
+        const zkey = await snarkjs.zKey.exportJson(completePathZkey);
+        //console.log('reading zkey: : ', zkey);
+        const res = await snarkjs.groth16.verify(vKey, publicSignals, proof);
+        if (res === true) {
+          console.log("Verification OK");
+        } else {
+          console.log("Invalid proof");
+          throw new Error("Invalid Proof");
+        }
     })
 
     it("example poseidon hash", async () => {
