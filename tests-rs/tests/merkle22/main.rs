299b3473033e9d2cfede449301a56eb798ce4608
Sergey Timoshin <sergeytimoshin@proton.me>
2024-02-14 14:47:55 +0000

    use relative path in test

--- tests-rs/tests/merkle22/main.rs
+++ tests-rs/tests/merkle22/main.rs
@@ -1,15 +1,15 @@
 use tests_rs::{merkle_tree_inputs, prepare_inputs, prove, verify};
 const ZK_BYTES: &[&[u8]; 10] = &[
-    include_bytes!("/Users/tsv/Developer/circuit-template/build/merkle22_1/circuit.zkey"),
-    include_bytes!("/Users/tsv/Developer/circuit-template/build/merkle22_2/circuit.zkey"),
-    include_bytes!("/Users/tsv/Developer/circuit-template/build/merkle22_3/circuit.zkey"),
-    include_bytes!("/Users/tsv/Developer/circuit-template/build/merkle22_4/circuit.zkey"),
-    include_bytes!("/Users/tsv/Developer/circuit-template/build/merkle22_5/circuit.zkey"),
-    include_bytes!("/Users/tsv/Developer/circuit-template/build/merkle22_6/circuit.zkey"),
-    include_bytes!("/Users/tsv/Developer/circuit-template/build/merkle22_7/circuit.zkey"),
-    include_bytes!("/Users/tsv/Developer/circuit-template/build/merkle22_8/circuit.zkey"),
-    include_bytes!("/Users/tsv/Developer/circuit-template/build/merkle22_9/circuit.zkey"),
-    include_bytes!("/Users/tsv/Developer/circuit-template/build/merkle22_10/circuit.zkey"),
+    include_bytes!("../../..//build/merkle22_1/circuit.zkey"),
+    include_bytes!("../../..//build/merkle22_2/circuit.zkey"),
+    include_bytes!("../../..//build/merkle22_3/circuit.zkey"),
+    include_bytes!("../../..//build/merkle22_4/circuit.zkey"),
+    include_bytes!("../../..//build/merkle22_5/circuit.zkey"),
+    include_bytes!("../../..//build/merkle22_6/circuit.zkey"),
+    include_bytes!("../../..//build/merkle22_7/circuit.zkey"),
+    include_bytes!("../../..//build/merkle22_8/circuit.zkey"),
+    include_bytes!("../../..//build/merkle22_9/circuit.zkey"),
+    include_bytes!("../../..//build/merkle22_10/circuit.zkey"),
 ];
 
 #[test]
@@ -17,7 +17,7 @@ fn test_prove_and_very_1_to_10() {
     let indices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     for (i, el) in indices.iter().enumerate() {
         println!("Proving merkle22_{}...", el);
-        let wasm_path = format!("/Users/tsv/Developer/circuit-template/build/merkle22_{}/merkle22_{}_js/merkle22_{}.wasm", el, el, el);
+        let wasm_path = format!("../build/merkle22_{}/merkle22_{}_js/merkle22_{}.wasm", el, el, el);
         let zk_bytes: &[u8] = ZK_BYTES[i];
         let inputs = vec![merkle_tree_inputs(); i+1];
         let proof_inputs = prepare_inputs(&inputs, zk_bytes, &wasm_path);
